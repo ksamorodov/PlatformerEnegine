4d3e914f9c7434776eded0f04b45ea0324a47d48
ksamorodov <37376734+ksamorodov@users.noreply.github.com>
2020-09-19 16:15:00 +0300

    Add class GameObject

---
+++ Platformerengine/Platformerengine/res/code/physics/IVector2d.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Platformerengine.res.code.logic {
+    interface IVector2d {
+        IVector2d VectorAddition(Vector2d vec2);
+        IVector2d MultiplyVector(Vector2d vec2);
+    }
+}
---
+++ Platformerengine/Platformerengine/res/code/physics/Vector2d.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Platformerengine.res.code.logic {
+    class Vector2d : IVector2d {
+        private int dX { get; }
+        private int dY { get; }
+        public Vector2d(int _dx, int _dy) {
+            dX = _dx;
+            dY = _dy;
+        }
+
+        public IVector2d VectorAddition(Vector2d vec2) {
+            throw new NotImplementedException();
+        }
+
+        public IVector2d MultiplyVector(Vector2d vec2) {
+            throw new NotImplementedException();
+        }
+    }
+}
